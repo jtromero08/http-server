0840ddfb68ac2c8c357cb9746426d99a5cde64de
jtromero08 <jtromero08@gmail.com>
2024-06-02 19:57:26 -0300

    test

--- app/main.ts
+++ app/main.ts
@@ -19,7 +19,7 @@ const server = net.createServer((socket) => {
         if(path[1] === '/user-agent')
             socket.write(`${httpResponse200WithContent} ${agentRequest.length}\r\n\r\n${agentRequest}`)
         if(path[1] === `/files/${echoRequest}`)
-            socket.write(`${httpResponse200WithContent} ${agentRequest.length}\r\n\r\n${agentRequest}`)
+            socket.write(`${httpResponse200WithContent} ${echoRequest.length}\r\n\r\n${agentRequest}`)
         if(path[1] !== '/')
             socket.write(`HTTP/1.1 ${Status[404].code.toString()} ${Status[404].message}\r\n\r\n`)
 
