45e458b09a9b3a254fc7a8d0cfa023026c214b6b
jtromero08 <jtromero08@gmail.com>
2024-06-01 22:05:39 -0300

    test

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4221
 
 const server = net.createServer((socket) => {
     socket.on('data', data => {
-        socket.write(data)
+        socket.write(data.toString())
     })
     socket.end();
 });
