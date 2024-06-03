ae70ef5ae587bb62ac96521d83b98a166f6dd80a
jtromero08 <jtromero08@gmail.com>
2024-06-03 18:18:11 -0300

    test

--- app/main.ts
+++ app/main.ts
@@ -45,6 +45,7 @@ const server = net.createServer((socket) => {
         if(path[1] === `/files/${echoRequest}` && method === Methods.POST) {
             console.log('Enter to file condition')
             try {
+                console.log(request[3])
                 const fileContent = fs.writeFileSync(fileName, request[3])
                 socket.write(
                     `${httpResponse200sWithContent} ${request[3].length}\r\n\r\n${request[3]}`
