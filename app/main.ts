b58572e248b3d981597ab478a0180d8f9d839947
jtromero08 <jtromero08@gmail.com>
2024-06-03 06:57:25 -0300

    test

--- app/main.ts
+++ app/main.ts
@@ -16,7 +16,7 @@ const server = net.createServer((socket) => {
         }
         const contentType = (path[1]===`/files/${echoRequest}`) ? ContentTypes.application : ContentTypes.text
         const httpResponse200WithContent = `HTTP/1.1 ${Status[200].code.toString()} ${Status[200].message}\r\nContent-Type: ${contentType}\r\nContent-Length:`
-        const fileName = `${process.argv[3]}${echoRequest}`
+        const fileName = `${process.argv[3]}${echoRequest}.txt`
         console.log('This file name: ', fileName)
         const fileContent = fs.readFileSync(fileName)
         console.log('This file content: ', fileContent)
