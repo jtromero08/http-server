b29bcd379668845ce285a225e04d89210aea414b
jtromero08 <jtromero08@gmail.com>
2024-06-03 22:06:29 -0300

    Pass 8 stage

--- app/main.ts
+++ app/main.ts
@@ -22,8 +22,6 @@ const server = net.createServer((socket) => {
         const httpResponse200sWithContent = `HTTP/1.1 ${statusCodeRequest.code} ${statusCodeRequest.message}\r\nContent-Type: ${contentType}\r\nContent-Length:`
         const httpResponse404 = `HTTP/1.1 ${Status[404].code.toString()} ${Status[404].message}\r\n\r\n`;
         const fileName = `${process.argv[3]}${echoRequest}`
-        const buffer = Buffer.from(echoRequest, 'utf-8');
-        const zipper = zlib.gzipSync(buffer)
 
         if(path[1] === '/') 
             socket.write(`HTTP/1.1 ${Status[200].code.toString()} ${Status[200].message}\r\n\r\n`)
@@ -31,6 +29,8 @@ const server = net.createServer((socket) => {
             socket.write(`${httpResponse200sWithContent} ${agentRequest.length}\r\n\r\n${agentRequest}`)
         if(path[1] === `/echo/${echoRequest}`) {
             if(acceptedEncoding.includes('gzip,') || acceptedEncoding[1] === 'gzip') {
+                const buffer = Buffer.from(echoRequest, 'utf-8');
+                const zipper = zlib.gzipSync(buffer)
                 socket.write(
                     `HTTP/1.1 ${statusCodeRequest.code} ${statusCodeRequest.message}\r\nContent-Encoding: gzip\r\nContent-Type: ${contentType}\r\nContent-Length: ${zipper.length}\r\n\r\n`
                 )
