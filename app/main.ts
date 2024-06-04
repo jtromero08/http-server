ecd48caa5b0d4646f28341cb0f8a4f2baa8c716d
jtromero08 <jtromero08@gmail.com>
2024-06-03 22:09:57 -0300

    Pass 8 stage

--- app/main.ts
+++ app/main.ts
@@ -70,10 +70,8 @@ const server = net.createServer((socket) => {
     })
 });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 server.listen(PORT as number, 'localhost', () => {
     console.log(`Server is running on port ${PORT}`);
 });
